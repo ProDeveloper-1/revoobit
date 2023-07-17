1e7d09fc0e0bac4d5046919eb51f819fb2c63232
ProDeveloper-1 <prospermbuma@gmail.com>
2023-07-17 23:45:46 +0300

    More beautiful updates

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,26 +1,86 @@
 /*=================================================
 # FAQ
 ==================================================*/
-$(document).ready(function() {
+$(document).ready(function () {
 
     $(".answer").hide();
+    $(".fa-angle-up").hide();
 
-    $(".question_1").click(function(){
-        $(".answer_1").slideToggle("slow");
+    // Question 1
+    $(".question_1 .fa-angle-down").click(function () {
+        $(".answer_1").slideDown("slow");
+        $(".question_1 .fa-angle-down").hide();
+        $(".question_1 .fa-angle-up").show();
     });
-    $(".question_2").click(function(){
-        $(".answer_2").slideToggle("slow");
+
+    $(".question_1 .fa-angle-up").click(function () {
+        $(".answer_1").slideUp("slow");
+        $(".question_1 .fa-angle-down").show();
+        $(".question_1 .fa-angle-up").hide();
     });
-    $(".question_3").click(function(){
-        $(".answer_3").slideToggle("slow");
+
+    // Question 2
+    $(".question_2 .fa-angle-down").click(function () {
+        $(".answer_2").slideDown("slow");
+        $(".question_2 .fa-angle-down").hide();
+        $(".question_2 .fa-angle-up").show();
     });
-    $(".question_4").click(function(){
-        $(".answer_4").slideToggle("slow");
+
+    $(".question_2 .fa-angle-up").click(function () {
+        $(".answer_2").slideUp("slow");
+        $(".question_2 .fa-angle-down").show();
+        $(".question_2 .fa-angle-up").hide();
     });
-    $(".question_5").click(function(){
-        $(".answer_5").slideToggle("slow");
+
+    // Question 3
+    $(".question_3 .fa-angle-down").click(function () {
+        $(".answer_3").slideDown("slow");
+        $(".question_3 .fa-angle-down").hide();
+        $(".question_3 .fa-angle-up").show();
     });
-    $(".question_6").click(function(){
-        $(".answer_6").slideToggle("slow");
+
+    $(".question_3 .fa-angle-up").click(function () {
+        $(".answer_3").slideUp("slow");
+        $(".question_3 .fa-angle-down").show();
+        $(".question_3 .fa-angle-up").hide();
+    });
+
+    // Question 4
+    $(".question_4 .fa-angle-down").click(function () {
+        $(".answer_4").slideDown("slow");
+        $(".question_4 .fa-angle-down").hide();
+        $(".question_4 .fa-angle-up").show();
+    });
+
+    $(".question_4 .fa-angle-up").click(function () {
+        $(".answer_4").slideUp("slow");
+        $(".question_4 .fa-angle-down").show();
+        $(".question_4 .fa-angle-up").hide();
+    });
+
+    // Question 5
+    $(".question_5 .fa-angle-down").click(function () {
+        $(".answer_5").slideDown("slow");
+        $(".question_5 .fa-angle-down").hide();
+        $(".question_5 .fa-angle-up").show();
+    });
+
+    $(".question_5 .fa-angle-up").click(function () {
+        $(".answer_5").slideUp("slow");
+        $(".question_5 .fa-angle-down").show();
+        $(".question_5 .fa-angle-up").hide();
+    });
+
+    // Question 6
+    $(".question_6 .fa-angle-down").click(function () {
+        $(".answer_6").slideDown("slow");
+        $(".question_6 .fa-angle-down").hide();
+        $(".question_6 .fa-angle-up").show();
+    });
+
+    $(".question_6 .fa-angle-up").click(function () {
+        $(".answer_6").slideUp("slow");
+        $(".question_6 .fa-angle-down").show();
+        $(".question_6 .fa-angle-up").hide();
     });
 });
