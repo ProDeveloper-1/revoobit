341c5dd4e3da631fe377c2456cb63a02b59779df
ProDeveloper-1 <prospermbuma@gmail.com>
2023-07-17 17:13:13 +0300

    More

---
+++ assets/js/faq.js
@@ -0,0 +1,26 @@
+/*=================================================
+# FAQ
+==================================================*/
+$(document).ready(function() {
+
+    $(".answer").hide();
+
+    $(".question_1").click(function(){
+        $(".answer_1").slideToggle("slow");
+    });
+    $(".question_2").click(function(){
+        $(".answer_2").slideToggle("slow");
+    });
+    $(".question_3").click(function(){
+        $(".answer_3").slideToggle("slow");
+    });
+    $(".question_4").click(function(){
+        $(".answer_4").slideToggle("slow");
+    });
+    $(".question_5").click(function(){
+        $(".answer_5").slideToggle("slow");
+    });
+    $(".question_6").click(function(){
+        $(".answer_6").slideToggle("slow");
+    });
+});
