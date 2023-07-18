53d2db5a345902d3a85bc9d96403435f26c28eb3
ProDeveloper-1 <prospermbuma@gmail.com>
2023-07-18 17:05:12 +0300

    More updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,26 +35,6 @@ window.addEventListener('scroll', () => {
 });
 
 /* ==================================================
-# Initialize Swiper
-===================================================*/
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-
-/* ==================================================
 #  Initialize AOS
 ===================================================*/
 AOS.init({
